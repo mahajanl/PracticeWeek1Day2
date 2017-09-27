96b3d8814ebe842a0d26d97c66b71306d9adbb24
mahajanl <lakshamima@gmail.com>
2017-09-27 11:28:33 -0400

    initial commit for homework

---
+++ PracticeWeek1Day2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWeek1Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPeople = 3;
+
+            int billPerPerson = 13;
+
+            Console.WriteLine("The total bill for all 3 people was " + numberPeople* billPerPerson);
+
+
+
+        }
+    }
+}
